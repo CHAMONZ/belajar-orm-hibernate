b73df192bd9b6724322fe804e856e59510423c22
dimmaryanto <dimmaryanto@gmail.com>
2015-09-12 13:59:24 +0700

    membuat model class

---
+++ src/main/java/org/unikom/dimmaryanto/dto/UserDetails.java
@@ -0,0 +1,27 @@
+package org.unikom.dimmaryanto.dto;
+
+/**
+ * Created by dimmaryanto on 9/12/15.
+ */
+
+public class UserDetails {
+
+    private int id;
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
