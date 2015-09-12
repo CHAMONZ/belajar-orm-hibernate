f7502c39940ddbd4186245734acfc2bfa6a22027
dimmaryanto <dimmaryanto@gmail.com>
2015-09-12 15:33:32 +0700

    menerima data dari database

--- src/main/java/org/unikom/dimmaryanto/hibernate/HibernateTest.java
+++ src/main/java/org/unikom/dimmaryanto/hibernate/HibernateTest.java
@@ -6,6 +6,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.unikom.dimmaryanto.dto.UserDetails;
 
+import java.util.Date;
+
 /**
  * Created by dimmaryanto on 9/12/15.
  */
@@ -13,9 +15,10 @@ public class HibernateTest {
     public static void main(String[] args) {
         UserDetails aUser = new UserDetails();
 
-        aUser.setId(1);
-        aUser.setUsername("Dimas Maryanto");
-
+        aUser.setId(3);
+        aUser.setUsername("Riansyah Permana Putra");
+        aUser.setAddress("Jl.Cikutra");
+        aUser.setJoinedDate(new Date());
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
@@ -24,6 +27,16 @@ public class HibernateTest {
         session.save(aUser);
         session.beginTransaction().commit();
         session.close();
+
+        aUser = null;
+
+        session = sessionFactory.openSession();
+
+        session.beginTransaction();
+        aUser = session.get(UserDetails.class, 3);
+
+        System.out.println(aUser.getUsername() + " is a user recieved from table USER_DETAILS");
+
         sessionFactory.close();
     }
 }
