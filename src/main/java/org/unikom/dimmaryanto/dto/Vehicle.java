747279e55e841af92209b2c0c4fd45363c185325
dimmaryanto <dimmaryanto@gmail.com>
2015-09-13 15:34:11 +0700

    menambah model vehicle

---
+++ src/main/java/org/unikom/dimmaryanto/dto/Vehicle.java
@@ -0,0 +1,34 @@
+package org.unikom.dimmaryanto.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by dimmaryanto on 9/13/15.
+ */
+@Entity
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int vehicleId;
+    private String vehicleName;
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+}
