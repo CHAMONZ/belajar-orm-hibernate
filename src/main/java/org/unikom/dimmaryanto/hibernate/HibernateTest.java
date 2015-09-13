c956b0993c07848d34aec07c02f3c282d16d5d23
dimmaryanto <dimmaryanto@gmail.com>
2015-09-13 20:41:53 +0700

    menggunakan HQL

--- src/main/java/org/unikom/dimmaryanto/hibernate/HibernateTest.java
+++ src/main/java/org/unikom/dimmaryanto/hibernate/HibernateTest.java
@@ -1,9 +1,14 @@
 
 package org.unikom.dimmaryanto.hibernate;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.unikom.dimmaryanto.dto.UserDetails;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by dimmaryanto on 9/12/15.
@@ -17,11 +22,22 @@ public class HibernateTest {
 
         session.beginTransaction();
 
+        //"from UserDetails" UserDetails -> bukan merupakan nama Table tetapi nama Object Class Entity
+        Query query = session.createQuery("from UserDetails");
+        List<UserDetails> users = query.list();
 
         session.beginTransaction().commit();
         session.close();
         sessionFactory.close();
 
+
+        Iterator<UserDetails> iterator = users.iterator();
+
+        while (iterator.hasNext()) {
+            //untuk mengambil object UserDetails
+            UserDetails user = iterator.next();
+            System.out.println("User ID : " + user.getId() + " and Username : " + user.getUsername());
+        }
 
     }
 }
