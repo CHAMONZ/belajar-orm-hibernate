9187ae6807e91c6e3922c4f67dc3d1367d019a00
dimmaryanto <dimmaryanto@gmail.com>
2015-09-13 22:51:43 +0700

    pengenalan Criteria dari package org.hibernate.Criteria

--- src/main/java/org/unikom/dimmaryanto/hibernate/HibernateTest.java
+++ src/main/java/org/unikom/dimmaryanto/hibernate/HibernateTest.java
@@ -1,9 +1,14 @@
 
 package org.unikom.dimmaryanto.hibernate;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.unikom.dimmaryanto.dto.UserDetails;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by dimmaryanto on 9/12/15.
@@ -17,6 +22,18 @@ public class HibernateTest {
 
         session.beginTransaction();
 
+        Criteria criteria = session.createCriteria(UserDetails.class);
+
+
+        List<UserDetails> listOfUserDetailses = criteria.list();
+
+
+        Iterator<UserDetails> iterator = listOfUserDetailses.iterator();
+
+        while (iterator.hasNext()) {
+            UserDetails user = iterator.next();
+            System.out.println("Username is " + user.getUsername() + " user id is " + user.getId());
+        }
 
         session.beginTransaction().commit();
         session.close();
