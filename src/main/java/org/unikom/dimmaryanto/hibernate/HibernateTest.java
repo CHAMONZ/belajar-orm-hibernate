1798614bd91938abf99c9dfb090bd27e5b9e39a4
dimmaryanto <dimmaryanto@gmail.com>
2015-09-13 18:40:45 +0700

    hapus simpan data

--- src/main/java/org/unikom/dimmaryanto/hibernate/HibernateTest.java
+++ src/main/java/org/unikom/dimmaryanto/hibernate/HibernateTest.java
@@ -4,7 +4,6 @@ package org.unikom.dimmaryanto.hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.unikom.dimmaryanto.dto.UserDetails;
 
 /**
  * Created by dimmaryanto on 9/12/15.
@@ -18,11 +17,6 @@ public class HibernateTest {
 
         session.beginTransaction();
 
-        for (int i = 0; i < 10; i++) {
-            UserDetails aUserDetails = new UserDetails();
-            aUserDetails.setUsername("User ke " + i);
-            session.save(aUserDetails);
-        }
 
         session.beginTransaction().commit();
         session.close();
