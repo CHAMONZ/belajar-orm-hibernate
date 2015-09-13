3619046ab9409d77434208b28a93eac39b875f3d
dimmaryanto <dimmaryanto@gmail.com>
2015-09-13 18:39:10 +0700

    menyimpan data UserDetails sebayak 10x

--- src/main/java/org/unikom/dimmaryanto/hibernate/HibernateTest.java
+++ src/main/java/org/unikom/dimmaryanto/hibernate/HibernateTest.java
@@ -4,6 +4,7 @@ package org.unikom.dimmaryanto.hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.unikom.dimmaryanto.dto.UserDetails;
 
 /**
  * Created by dimmaryanto on 9/12/15.
@@ -16,6 +17,12 @@ public class HibernateTest {
         Session session = sessionFactory.openSession();
 
         session.beginTransaction();
+
+        for (int i = 0; i < 10; i++) {
+            UserDetails aUserDetails = new UserDetails();
+            aUserDetails.setUsername("User ke " + i);
+            session.save(aUserDetails);
+        }
 
         session.beginTransaction().commit();
         session.close();
